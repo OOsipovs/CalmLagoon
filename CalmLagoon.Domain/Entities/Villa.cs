40b5042156444e171a243804c6dc1cee3cd8b0f8
olegs.osipovs <olegs.osipovs@gmail.com>
2024-06-16 17:03:12 +0300

    Clean Infrastructure initialized

---
+++ CalmLagoon.Domain/Entities/Villa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalmLagoon.Domain.Entities
+{
+    public class Villa
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public int Sqrtf { get; set; }
+        public int Occupacy { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public DateTime? UpdatedDate { get; set; }
+    }
+}
